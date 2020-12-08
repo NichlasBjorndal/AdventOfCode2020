9bf201b21c49cf9216f445c4ce97c564a959eefa
Nichlas <Nichl@DESKTOP-RIF0RFT>
2020-12-08 22:05:16 +0100

    day 2 challenge

---
+++ December2/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace December2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int partOnecounter = 0;
+            int partTwocounter = 0;
+
+
+            string line;
+
+            // Read the file and display it line by line.  
+            string filePath = System.IO.Path.GetFullPath("input.txt");
+            System.IO.StreamReader file =
+                new System.IO.StreamReader(filePath);
+            while ((line = file.ReadLine()) != null)
+            {
+                var lineParts = line.Split(':');
+
+                var limits = lineParts[0].Split('-');
+                var lowerLimit = int.Parse(limits[0]);
+
+                var secondPartOfLimit = limits[1].Split(' ');
+                var upperLimit = int.Parse(secondPartOfLimit[0]);
+
+                char delimiter = secondPartOfLimit[1].ToCharArray()[0];
+
+                var password = lineParts[1].Trim();
+
+
+                var count = password.Count(x => x == delimiter);
+
+                if(count >= lowerLimit && count <= upperLimit)
+                {
+                    partOnecounter++;
+                }
+
+                if (password[lowerLimit-1] == delimiter ^ password[upperLimit - 1] == delimiter)
+                {
+                    partTwocounter++;
+                }
+
+
+            }
+
+            file.Close();
+
+            Console.WriteLine($"Part 1: There is current {partOnecounter} correct passwords");
+            Console.WriteLine($"Part 2: There is current {partTwocounter} correct passwords");
+        }
+
+    }
+}
